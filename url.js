50a8edbba64acbe1b8575ec73e9154ce111478df
Jiayu809 <tracy910809@gmail.com>
2026-09-26 16:28:19 +0800

    Clean up URL list in url.js
    Removed duplicate URLs and fixed formatting issues.

--- url.js
+++ url.js
@@ -9,10 +9,10 @@
 
 const urls = [
 	'https://www.surveycake.com/s/N9GVL',
-	'https://www.surveycake.com/s/Z2a9G'
-	'https://www.surveycake.com/s/Z2a9G,
-	'https://www.surveycake.com/s/Z2a9G,
-	'https://www.surveycake.com/s/Z2a9G,
-	'https://www.surveycake.com/s/Z2a9G,
-	'https://www.surveycake.com/s/Z2a9G,
+	'https://www.surveycake.com/s/Z2a9G',
+	'https://www.surveycake.com/s/Z2a9G ,
+	'https://www.surveycake.com/s/Z2a9G ,
+	'https://www.surveycake.com/s/Z2a9G ,
+	'https://www.surveycake.com/s/Z2a9G ,
+	'https://www.surveycake.com/s/Z2a9G ,
 ];
